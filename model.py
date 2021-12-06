b8bb777508fed7c7d9e08d5ffaed4de76a6cda1d
MInJun <68832065+wkdalswns0427@users.noreply.github.com>
2021-12-06 20:35:03 +0900

    model commit

---
+++ model.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms.functional as F
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias = False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+class UNET(nn.Module):
+    def __init__(
+            self, in_channels = 3, out_channels = 1, features = [64, 128, 256, 512]
+    ):
+        super(UNET, self).__init__()
+        self.ups = nn.ModuleList()
+        self.downs = nn.ModuleList()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # downward processing // down from input ro 512 in unet structure
+        for feature in features:
+            self.downs.append(DoubleConv(in_channels, feature))
+            in_channels = feature
+
+        # upward processing
+        for feature in reversed(features):
+            self.ups.append(
+                nn.ConvTranspose2d(feature*2, feature, kernel_size = 2, stride = 2,)
+            )
+            self.ups.append(DoubleConv(feature*2, feature))
+
+        self.bottleneck = DoubleConv(features[-1], features[-1]*2)
+        self.final_conv = nn.COnv2d(features[0], out_channels, kernel_size = 1)
+    def forward(self, x):
+        skip_connections = []
+        for down in self.downs:
+            x = down(x)
+            skip_connections.append(x)
+            x = self.pool(x)
+
+        x = self.bottleneck(x)
+        skip_connections = skip_connections[::-1]
+
+        for idx in range(0, len(self, ups),2):
