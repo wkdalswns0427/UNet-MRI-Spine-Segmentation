92d994ad996dd65f676a75ba743924cfae4c413c
MInJun <68832065+wkdalswns0427@users.noreply.github.com>
2021-12-06 21:12:51 +0900

    model commit

--- model.py
+++ model.py
@@ -6,10 +6,10 @@ class DoubleConv(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(DoubleConv, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias = False),
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
         )
@@ -19,7 +19,7 @@ class DoubleConv(nn.Module):
 
 class UNET(nn.Module):
     def __init__(
-            self, in_channels = 3, out_channels = 1, features = [64, 128, 256, 512]
+            self, in_channels = 3, out_channels = 1, features = [64, 128, 256, 512],
     ):
         super(UNET, self).__init__()
         self.ups = nn.ModuleList()
@@ -39,9 +39,11 @@ class UNET(nn.Module):
             self.ups.append(DoubleConv(feature*2, feature))
 
         self.bottleneck = DoubleConv(features[-1], features[-1]*2)
-        self.final_conv = nn.COnv2d(features[0], out_channels, kernel_size = 1)
+        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size = 1)
+
     def forward(self, x):
         skip_connections = []
+
         for down in self.downs:
             x = down(x)
             skip_connections.append(x)
@@ -50,4 +52,25 @@ class UNET(nn.Module):
         x = self.bottleneck(x)
         skip_connections = skip_connections[::-1]
 
-        for idx in range(0, len(self, ups),2):
+        for idx in range(0, len(self.ups), 2):
+            x = self.ups[idx](x)
+            skip_connection = skip_connections[idx//2]
+
+            if x.shape != skip_connection.shape:
+                x = F.resize(x, size=skip_connection.shape[2:])
+
+            concat_skip = torch.cat((skip_connection, x), dim=1)
+            x = self.ups[idx+1](concat_skip)
+
+        return self.final_conv(x)
+
+def test():
+    x = torch.randn((3, 1, 161, 161))
+    model = UNET(in_channels=1, out_channels=1)
+    preds = model(x)
+    print(preds.shape)
+    print(x.shape)
+    assert preds.shape == x.shape
+
+if __name__=="__main__":
+    test()
