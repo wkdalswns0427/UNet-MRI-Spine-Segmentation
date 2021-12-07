0bb02d16ad08ae0dab564d040474e37da40e320b
MInJun <68832065+wkdalswns0427@users.noreply.github.com>
2021-12-07 12:42:32 +0900

    211207 commit

--- dataset.py
+++ dataset.py
@@ -1,12 +1,12 @@
 import os
-from PIL import Image
 import pydicom as dcm
 import pylibjpeg
 import numpy as np
+import scipy as io
 from torch.utils.data import Dataset
 
-
-
+# image in dcm format
+# label in matlab dictionary format
 class SpineDataset(Dataset):
     def __init__(self, image_dir, mask_dir, transform=None):
         self.image_dir = image_dir
@@ -19,8 +19,17 @@ class SpineDataset(Dataset):
 
     def __getitem__(self, index):
         img_path = os.path.join(self.image_dir, self.images[index])
-        mask_path = os.path.join(self.mask_dir, self.images[index].replace(".dcm","_mat"))
+        mask_path = os.path.join(self.mask_dir, self.images[index].replace(".dcm", ".mat"))
         rawimage = dcm.dcmread(img_path).pixel_array
         image_uns = rawimage.astype(float)
         image = (np.maximum(image_uns,0)/image_uns.max())*255
-        image = np.uint8(image)
+        image = np.uint8(image) # image
+        raw_mask = io.loadmat(mask_path)
+        mask = raw_mask['label_separated'][:,:,0] # 0~6 channels 0~5:each spine, 6:full spine
+
+        if self.transform is not None:
+            augmentations = self.transform(image=image, mask=mask)
+            image = augmentations["image"]
+            mask = augmentations["mask"]
+
+        return image, mask
