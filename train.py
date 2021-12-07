710ad1e5561ce7230648a21192ffb50c62862b5e
MInJun <68832065+wkdalswns0427@users.noreply.github.com>
2021-12-07 16:44:10 +0900

    211207 commit4

--- train.py
+++ train.py
@@ -28,12 +28,58 @@ TRAIN_MASK_DIR = "Train/label/"
 VAL_IMG_DIR = "Test/img/"
 # VAL_MASK_DIR = "data/val_masks/"
 
-def train_fn(loader, model, optimizer, loss_fn, scalar):
+def train_fn(loader, model, optimizer, loss_fn, scaler):
     loop = tqdm(loader)
+
+    for batch_idx, (data, targets) in enumerate(loop):
+        data = data.to(device=DEVICE)
+        targets = targets.float().unsqueeze(1).to(device=DEVICE)
+
+        # forward -- float16
+        with torch.cuda.amp.autocast():
+            predictions = model(data)
+            loss = loss_fn(predictions, targets)
+
+        # backward
+        optimizer.zero_grad()
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update(0)
+
+        # update tqdm loop
+        loop.set_postfix(loss=loss.item())
 
 
 def main():
-    pass
+    train_transform = A.Compose(
+        [
+            A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+            A.Rotate(limit=35, p=1.0),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.1),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_value=255.0,
+            ),
+            ToTensorV2(),
+        ],
+    )
+
+    val_transforms = A.Compose(
+        [
+            A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_value=255.0,
+            ),
+            ToTensorV2(),
+        ],
+    )
+
+    model = UNet(in_channels=3, out_channels=1).to(DEVICE) # change out_channel for multi classes
+    loss_fn = nn.BCEWithLogitsLoss()  # cross entropy loss for multiple classes
 
 if __name__ == "__main__":
     main()
