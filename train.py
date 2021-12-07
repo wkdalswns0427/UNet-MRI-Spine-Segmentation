3a15595676b66ae11cee4f40728e237cbfe729c6
MInJun <68832065+wkdalswns0427@users.noreply.github.com>
2021-12-07 13:11:36 +0900

    211207 commit2

--- train.py
+++ train.py
@@ -12,3 +12,18 @@ from model import UNet
 #     check_accuracy,
 #     save_predictions_as_imgs,
 # )
+
+# Hyperparameters and directories
+LEARNING_RATE = 1e-4
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+BATCH_SIZE = 16
+NUM_EPOCHS = 3
+NUM_WORKERS = 2
+IMAGE_HEIGHT = 160  # 1280 originally
+IMAGE_WIDTH = 240  # 1918 originally
+PIN_MEMORY = True
+LOAD_MODEL = False
+TRAIN_IMG_DIR = "Train/img/"
+TRAIN_MASK_DIR = "Train/label/"
+VAL_IMG_DIR = "data/val_images/"
+VAL_MASK_DIR = "data/val_masks/"
