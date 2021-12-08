a4a7022c16703eafcb594ddafa9a1d2721638d39
MInJun <68832065+wkdalswns0427@users.noreply.github.com>
2021-12-09 01:00:33 +0900

    is everything okay

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ from utils import (
 LEARNING_RATE = 1e-4
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 BATCH_SIZE = 16
-NUM_EPOCHS = 5
+NUM_EPOCHS = 3
 NUM_WORKERS = 2
 IMAGE_HEIGHT = 160  # 1280 originally
 IMAGE_WIDTH = 240  # 1918 originally
